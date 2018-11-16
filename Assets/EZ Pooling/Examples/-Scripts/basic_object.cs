a0cf2af3d609333bc9d9d3f3d29a110d5f84cad3
FranGhigli <44591120+FranGhgili@users.noreply.github.com>
2018-11-16 10:39:04 -0300

    Add files via upload

---
+++ Assets/EZ Pooling/Examples/-Scripts/basic_object.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using EZ_Pooling;
+
+public class basic_object : MonoBehaviour
+{
+    public GameObject spawner;
+
+    public GameObject recycler;
+
+    public float rndLimit;
+
+    void OnSpawned()
+    {
+
+        EZ_PoolManager.Spawn(spawner.transform, spawner.transform.position + transform.up * Mathf.RoundToInt(Random.Range(-rndLimit, rndLimit)), Quaternion.Euler (0, 0, 0));
+
+        if (GetComponent<Rigidbody>())
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        enabled = true;
+
+    }
+
+    void OnDespawned()
+    {
+        EZ_PoolManager.Despawn(recycler.transform);
+        enabled = false;
+
+    }
+}
